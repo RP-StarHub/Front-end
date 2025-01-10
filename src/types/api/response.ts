c2cb08e6d647aef2ea26750bdc6bf0995cd2b81d
devdaradara <devdaradara@gmail.com>
2025-01-10 14:59:56 +0900

    fix: API 응답 통일화에 따른 타입 변경 #8

--- src/types/api/response.ts
+++ src/types/api/response.ts
@@ -1,9 +1,8 @@
-// 현재는 응답이 다양하게 오는 상태라 추후 옵셔널 타입 변경 필요
 export interface ApiResponse<T> {
-  status?: number;
-  code?: string;
+  status: number;
+  code: string;
   data: T;
-  message?: string;
+  message: string;
 }
 
 // 추후 토큰 넣고 사용
