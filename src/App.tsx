c3f1ad5a5d84ea1c0fba4b07408814524bdc8fc9
devdaradara <devdaradara@gmail.com>
2025-01-14 10:47:05 +0900

    setting: react-query 클라이언트 설정 #8

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,30 @@ import Signup from "./pages/Signup";
 import ApplicantListPage from "./pages/ApplicantListPage";
 import StudyDetailPage from "./pages/StudyDetailPage";
 import StudyRecruitPage from "./pages/StudyRecruitPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/applicantlist/:postId" element={<ApplicantListPage />} />
-      <Route path="/studydetail/:postId" element={<StudyDetailPage />} />
-      <Route path="/studyrecruit" element={<StudyRecruitPage />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/applicantlist/:postId" element={<ApplicantListPage />} />
+        <Route path="/studydetail/:postId" element={<StudyDetailPage />} />
+        <Route path="/studyrecruit" element={<StudyRecruitPage />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
