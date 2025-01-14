431fae15afeee1d9811c1c5ee66b7b741106e233
devdaradara <devdaradara@gmail.com>
2025-01-14 14:11:33 +0900

    fix: react-query 클라이언트 최상위 컴포넌트로 옮기기 #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
@@ -6,30 +6,17 @@ import Signup from "./pages/Signup";
 import ApplicantListPage from "./pages/ApplicantListPage";
 import StudyDetailPage from "./pages/StudyDetailPage";
 import StudyRecruitPage from "./pages/StudyRecruitPage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-      staleTime: 1000 * 60 * 5,
-    },
-  },
-});
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/applicantlist/:postId" element={<ApplicantListPage />} />
-        <Route path="/studydetail/:postId" element={<StudyDetailPage />} />
-        <Route path="/studyrecruit" element={<StudyRecruitPage />} />
-      </Routes>
-    </QueryClientProvider>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/applicantlist/:postId" element={<ApplicantListPage />} />
+      <Route path="/studydetail/:postId" element={<StudyDetailPage />} />
+      <Route path="/studyrecruit" element={<StudyRecruitPage />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import './index.css';
 import App from './App';
@@ -8,20 +9,29 @@ import reportWebVitals from './reportWebVitals';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <App />
-      <Footer />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <App />
+        <Footer />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
