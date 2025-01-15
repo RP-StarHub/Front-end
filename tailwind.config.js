c77f01d43682b4381e0f9f1734942c572c657a9a
devdaradara <devdaradara@gmail.com>
2025-01-15 14:05:50 +0900

    style: 색상 및 타이포그래피 설정 #12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background": "#F6F1FB",
+        "yellow": "#FFE184",
+        "green": "#82C3B8",
+        "main": "#B3B4DC",
+        "sub": "#7C8BBE",
+        "bold": "#313866"
+      },
+      fontFamily: {
+        "gmarket-bold": ["GmarketSans", "sans-serif"],
+        "scdream4": ["SCDream4", "sans-serif"],
+        "scdream6": ["SCDream6", "sans-serif"]
+      },
+      fontSize: {
+        "page-title": ["30px", {
+          lineHeight: "1.5",
+          fontFamily: "GmarketSans"
+        }],
+        "button": ["24px", {
+          lineHeight: "1.5",
+          fontFamily: "SCDream6"
+        }],
+        "label": ["24px", {
+          lineHeight: "1.5",
+          fontFamily: "SCDream6"
+        }],
+        "placeholder": ["24px", {
+          lineHeight: "1.5",
+          fontFamily: "SCDream4"
+        }],
+        "regular": ["20px", {
+          lineHeight: "1.5",
+          fontFamily: "SCDream4"
+        }],
+      }
+    },
   },
   plugins: [],
 }
-
