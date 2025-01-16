328f751530b40182e1a666bc53a8269ae38f9d1d
devdaradara <devdaradara@gmail.com>
2025-01-16 09:36:14 +0900

    chore: 푸터 스타일 일부 변경 #12

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -4,40 +4,40 @@ import StarHubIconFooterbar from "../../../assets/icons/StarHubIconFooterbar.png
 const Footer = () => {
   return (
     <div className='w-full bg-bold flex flex-row justify-center items-center'>
-      <div className='text-center px-5 py-12'>
+      <div className='text-center px-40 py-12'>
         <img src={StarHubIconFooterbar} alt={'Logo'} style={{ width: 'auto', height: '150px' }} />
       </div>
-      <div className='text-center px-5 py-12'>
+      <div className='text-center px-40 py-12'>
         <div className='mb-4 text-label text-white font-gmarket-bold'>
           Delvelopers
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           박유나
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           김가빈
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           류지예
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           박유정
         </div>
       </div>
-      <div className='text-center px-5 py-12'>
+      <div className='text-center px-40 py-12'>
         <div className='mb-4 text-label text-white font-gmarket-bold'>
           Contact
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           yndbsk9372@gmail.com
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           kabin2205@gmail.com
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           devdaradara@gmail.com
         </div>
-        <div className='m-0 text-label font-scdream4 text-white'>
+        <div className='m-0 text-regular font-scdream4 text-white'>
           p_1321@duksung.ac.kr
         </div>
       </div>
