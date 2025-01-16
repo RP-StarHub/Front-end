e8f27992c4fee33f419b82cfc3c5d21ffbcb007f
devdaradara <devdaradara@gmail.com>
2025-01-16 09:30:30 +0900

    feat: 나브바 및 푸터바 tailwind 적용 #12

--- src/components/common/layout/Footer.tsx
+++ src/components/common/layout/Footer.tsx
@@ -1,58 +1,47 @@
 import React from 'react';
-import styled from "styled-components";
 import StarHubIconFooterbar from "../../../assets/icons/StarHubIconFooterbar.png";
 
-const FooterContainer = styled.div`
-  width: 100%;
-  background-color: #313866;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Section = styled.div`
-  text-align: center;
-  padding: 20px 50px 20px 50px;
-  margin: 0px 50px 0px 50px;
-`;
-
-const TitleContent = styled.p`
-  margin: 0px 0px 10px 0px;
-  font-size: 18px;
-  font-family: 'GmarketSans';
-  color: #fff;
-`;
-
-const TextContent = styled.p`
-  margin: 0;
-  font-size: 18px;
-  font-family: 'SCDream4';
-  color: #fff;
-`;
-
 const Footer = () => {
-
   return (
-    <FooterContainer>
-      <Section>
-          <img src={StarHubIconFooterbar} alt={'Logo'} style={{ width: 'auto', height: '150px'}} />
-      </Section>
-        <Section>
-          <TitleContent>Delvelopers</TitleContent>
-          <TextContent>박유나</TextContent>
-          <TextContent>김가빈</TextContent>
-          <TextContent>류지예</TextContent>
-          <TextContent>박유정</TextContent>
-        </Section>
-        <Section>
-          <TitleContent>Contact</TitleContent>        
-          <TextContent>yndbsk9372@gmail.com</TextContent>
-          <TextContent>kabin2205@gmail.com</TextContent>
-          <TextContent>devdaradara@gmail.com</TextContent>
-          <TextContent>p_1321@duksung.ac.kr</TextContent>
-        </Section>
-    </FooterContainer>
+    <div className='w-full bg-bold flex flex-row justify-center items-center'>
+      <div className='text-center px-5 py-12'>
+        <img src={StarHubIconFooterbar} alt={'Logo'} style={{ width: 'auto', height: '150px' }} />
+      </div>
+      <div className='text-center px-5 py-12'>
+        <div className='mb-4 text-label text-white font-gmarket-bold'>
+          Delvelopers
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          박유나
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          김가빈
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          류지예
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          박유정
+        </div>
+      </div>
+      <div className='text-center px-5 py-12'>
+        <div className='mb-4 text-label text-white font-gmarket-bold'>
+          Contact
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          yndbsk9372@gmail.com
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          kabin2205@gmail.com
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          devdaradara@gmail.com
+        </div>
+        <div className='m-0 text-label font-scdream4 text-white'>
+          p_1321@duksung.ac.kr
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/common/layout/Navbar.tsx
+++ src/components/common/layout/Navbar.tsx
@@ -1,34 +1,9 @@
 import React, { useState } from 'react';
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import StarHubIconNavbar from "../../../assets/icons/StarHubIconNavbar.png";
 import { User } from '../../../types';
 import { useLogout } from '../../../hooks/api/useUser';
 import { useQueryClient } from '@tanstack/react-query';
-
-const NavContainer = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px 20px;
-  background-color: #7C8BBE;
-  font-family: 'SCDream4';
-`;
-
-const TextLink = styled(Link)`
-  text-decoration: none;
-`
-
-const NavItems = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const NavItem = styled.div`
-  margin: 0 10px;
-  cursor: pointer;
-  color: #fff;
-`;
 
 const Navbar = () => {
   const [userInfo, setUserInfo] = useState<User | null>(
@@ -55,28 +30,53 @@ const Navbar = () => {
   };
 
   return (
-    <NavContainer>
-      <NavItems>
-        <TextLink to="/">
-          <img src={StarHubIconNavbar} alt={'Logo'} style={{ width: 'auto', height: '50px', marginRight: '10px', marginTop: '5px'}} />
-        </TextLink>
-        <TextLink to="/study/recruit">
-          <NavItem>모집하기</NavItem>
-        </TextLink>
-      </NavItems>
-      <NavItems>
-        {userInfo!==null ? (
-          <>
-            <NavItem>{userInfo.name} 님</NavItem>
-            <NavItem onClick={handleLogout}>로그아웃</NavItem>
-          </>
-        ) : (
-          <TextLink to="/login">
-            <NavItem>로그인</NavItem>
-          </TextLink>
-        )}
-      </NavItems>
-    </NavContainer>
+    <div className='bg-sub'>
+      {/**
+       * 나브바 크기 조절 속성 임시 비활성화 
+       * TODO: 메인페이지의 경우 크게, 다른 페이지의 경우 작게 나타나도록 수정
+       * className="max-w-screen-2xl
+       */}
+      <div className="mx-auto px-52 py-2 flex justify-between items-center">
+        <div className='flex items-center'>
+          <Link to="/">
+            <img 
+              src={StarHubIconNavbar} 
+              alt={'Logo'} 
+              // style={{ width: 'auto', height: '50px', marginRight: '10px', marginTop: '5px' }} 
+              className='cursor-pointer h-[70px] w-auto mr-5'
+            />
+          </Link>
+          <Link to="/study/recruit">
+            <span className='text-white font-scdream6 cursor-pointer mx-4 text-label'>
+              모집하기
+            </span>
+          </Link>
+        </div>
+        <div className='flex items-center'>
+          {userInfo !== null ? (
+            <>
+              <span className='text-white font-scdream6 cursor-pointer mx-4 text-label'>
+                {userInfo.name} 님
+              </span>
+              <span 
+                className='text-white font-scdream6 cursor-pointer mx-4 text-label'
+                onClick={handleLogout}
+              >
+                로그아웃
+              </span>
+            </>
+          ) : (
+            <Link to="/login">
+              <span
+                className='text-white font-scdream6 cursor-pointer mx-4 text-label'
+              >
+                로그인
+              </span>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
