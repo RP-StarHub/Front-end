bb83436114cdf7cf9496fdbc05f47be0db040737
devdaradara <devdaradara@gmail.com>
2025-01-21 10:19:51 +0900

    refactor: 서비스 목업 코드 수정 #16

--- src/hooks/api/useUser.ts
+++ src/hooks/api/useUser.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query"
 import { LoginUserRequest } from "../../types/api/user"
-import { userServices } from "../../services/api/user"
+import { userServices, mockUserService } from "../../services/api/user"
 
 export const useLogin = () => {
   return useMutation({
@@ -25,23 +25,3 @@ export const useLogout = () => {
       userServices.getLogout(loginId)
   });
 }
-
-// 목업용 서비스 코드
-const mockUserService = {
-  postRegister: async (form: FormData) => {
-    const info = JSON.parse(form.get('info') as string);
-
-    // 실제 API 구조 모방
-    return {
-      data: {
-        "status": 201,
-        "code": "SUCCESS_CREATE_USER",
-        "message": "사용자 회원가입을 성공했습니다.",
-        "data": {
-          "username": info.username,
-          "isProfileComplete": info.true,
-        }
-      }
-    }
-  }
-};
--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -28,3 +28,26 @@ export const userServices = {
     )
   }
 }
+
+/**
+ * 목업용 서비스 코드
+ * @description 실제 API 구현 전 임시로 사용하는 목업 서비스
+ */
+export const mockUserService = {
+  postRegister: async (form: FormData) => {
+    const info = JSON.parse(form.get('info') as string);
+
+    // 실제 API 구조 모방
+    return {
+      data: {
+        "status": 201,
+        "code": "SUCCESS_CREATE_USER",
+        "message": "사용자 회원가입을 성공했습니다.",
+        "data": {
+          "username": info.username,
+          "isProfileComplete": false,
+        }
+      }
+    }
+  }
+};
