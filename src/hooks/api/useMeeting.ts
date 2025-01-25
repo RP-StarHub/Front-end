293fb1749f52541309f6816090d787a5555f20a3
devdaradara <devdaradara@gmail.com>
2025-01-25 17:55:23 +0900

    chore: 모임 관련 훅 모킹 해제 #23

--- src/hooks/api/useMeeting.ts
+++ src/hooks/api/useMeeting.ts
@@ -3,19 +3,19 @@ import { GetMeetingListResponse } from "../../types/api/meeting"
 import { meetingService, mockMeetingService } from "../../services/api/meeting";
 
 export const useMeetingList = () => {
-  // return useQuery<GetMeetingListResponse>({
-  //   queryKey: ['meetings'],
-  //   queryFn: async () => {
-  //     const response = await meetingService.getMeetingList(0);
-  //     return response.data
-  //   }
-  // });
-
   return useQuery<GetMeetingListResponse>({
     queryKey: ['meetings'],
     queryFn: async () => {
-      const response = await mockMeetingService.getMeetingList(0);
+      const response = await meetingService.getMeetingList(0);
       return response.data
     }
   });
+
+  // return useQuery<GetMeetingListResponse>({
+  //   queryKey: ['meetings'],
+  //   queryFn: async () => {
+  //     const response = await mockMeetingService.getMeetingList(0);
+  //     return response.data
+  //   }
+  // });
 };
