df2d81a666d31bc13fb40984a7558bf7750c93e9
devdaradara <devdaradara@gmail.com>
2025-01-25 19:21:02 +0900

    feat: 로그인 안하고 좋아요 표시 토스트 메시지 추가 #23

--- src/hooks/api/useLike.ts
+++ src/hooks/api/useLike.ts
@@ -1,12 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { likeServices } from "../../services/api/like";
+import { toast } from "react-hot-toast";
 
 export const useLike = (meetingId: number) => {
   const queryClient = useQueryClient();
 
   const toggleLike = useMutation({
     mutationFn: (isLiked: boolean | null) => {
-      if (isLiked === null) return Promise.reject('Unauthorized');
+      if (isLiked === null) {
+        toast.error('로그인 후 관심 표시를 누를 수 있습니다.');
+        return Promise.reject('Unauthorized');
+      }
       return isLiked
         ? likeServices.deleteLikes(meetingId)
         : likeServices.postLikes(meetingId);
