8756820d207db83a6fdfc9f4cf086c722123fffb
devdaradara <devdaradara@gmail.com>
2025-01-26 21:28:54 +0900

    style: 지원서 없는 경우 문구 추가 #20

--- src/components/meeting/detail/ApplicationList.tsx
+++ src/components/meeting/detail/ApplicationList.tsx
@@ -27,6 +27,15 @@ const ApplicationList: React.FC<ApplicationListProps> = ({ meetingId }) => {
     toast.success(`${selectedApplication} 지원자 확정`);
   }
 
+  if (applications.length === 0) {
+    return (
+      <div className='flex flex-col items-center justify-center w-full py-20'>
+        <p className='font-scdream6 text-sub text-lg mb-2'>아직 지원서가 없습니다.</p>
+        <p className='font-scdream4 text-sub'>모임에 대한 자세한 소개를 적고 홍보하세요!</p>
+      </div>
+    );
+  }
+  
   return (
     <div className='w-full mt-8'>
       {applications.map((application) => (
