c97bea5c3431e283eba946d20e09abc51b34afb3
devdaradara <devdaradara@gmail.com>
2025-02-03 14:27:52 +0900

    feat: 마이페이지 상세 화면 UI 구현 #35

--- src/pages/mypage/AppliedMeetingsPage.tsx
+++ src/pages/mypage/AppliedMeetingsPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppliedMeetings } from '../../hooks/api/useMypage';
+import MeetingListLayout from '../../components/mypage/MeetingListLayout';
 
 function AppliedMeetingsPage() {
+  const [page, setPage] = useState(1);
+  const totalPages = 1;
+  const { data, isLoading } = useAppliedMeetings(page);
+
   return (
-    <div>AppliedMeetingsPage</div>
-  )
+    <MeetingListLayout
+      title="내가 참여한 모임"
+      meetings={data?.data.content || []}
+      isLoading={isLoading}
+      page={page}
+      totalPages={totalPages}
+      onPageChange={setPage}
+    />
+  );
 }
 
 export default AppliedMeetingsPage
--- src/pages/mypage/LikedMeetingsPage.tsx
+++ src/pages/mypage/LikedMeetingsPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useLikedMeetings } from '../../hooks/api/useMypage';
+import MeetingListLayout from '../../components/mypage/MeetingListLayout';
 
 function LikedMeetingsPage() {
+  const [page, setPage] = useState(1);
+  const totalPages = 1;
+  const { data, isLoading } = useLikedMeetings(page);
+
   return (
-    <div>LikedMeetingsPage</div>
-  )
+    <MeetingListLayout
+      title="내가 관심있는 글"
+      meetings={data?.data.content || []}
+      isLoading={isLoading}
+      page={page}
+      totalPages={totalPages}
+      onPageChange={setPage}
+    />
+  );
 }
 
-export default LikedMeetingsPage
+export default LikedMeetingsPage;
