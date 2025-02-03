cd61f5d5da47d4c575138a43cbb1a66c89058083
devdaradara <devdaradara@gmail.com>
2025-02-03 15:59:44 +0900

    style: 마이페이지 프로필 섹션 수정 #35

--- src/components/mypage/MyProfile.tsx
+++ src/components/mypage/MyProfile.tsx
@@ -23,32 +23,34 @@ const MyProfile: React.FC<MyProfileProps> = ({ profile }) => {
   }
 
   return (
-    <div className='flex bg-white rounded-lg px-40 py-10 box-border shadow-md'>
-      <div className='flex items-center justify-between w-full'>
-        <img
-          src={profile.profileImage}
-          alt='프로필 이미지'
-          className='w-32 h-32 rounded-full'
-        />
+    <div className='flex bg-white rounded-lg px-40 py-10 box-border shadow-md justify-center'>
+      <div className='flex w-2/3'>
+        <div className='flex items-start justify-between w-full'>
+          <img
+            src={profile.profileImage}
+            alt='프로필 이미지'
+            className='w-32 h-32 rounded-full self-center'
+          />
 
-        <div className='flex flex-col gap-4'>
-          <div className='flex items-center gap-4'>
-            <p className='text-page-title text-bold font-scdream6'>{profile.name}</p>
-            <p className='text-regular text-sub font-scdream4'>{profile.age}세</p>
+          <div className='flex flex-col gap-4'>
+            <div className='flex items-center gap-4'>
+              <p className='text-page-title text-bold font-scdream6'>{profile.name}</p>
+              <p className='text-regular text-sub font-scdream4'>{profile.age}세</p>
+            </div>
+            <p className='text-label text-bold font-scdream4'>{formatPhoneNumber(profile.phoneNumber)}</p>
+            <p className='text-label text-bold font-scdream4'>{profile.email}</p>
           </div>
-          <p className='text-label text-bold font-scdream4'>{formatPhoneNumber(profile.phoneNumber)}</p>
-          <p className='text-label text-bold font-scdream4'>{profile.email}</p>
-        </div>
 
-        <div className='flex flex-col gap-4'>
-          <p className='text-page-title text-white'>.</p>
-          <p className='text-label text-bold font-scdream4'>{profile.nickname}</p>
-          <p className='text-label text-bold font-scdream4'>{profile.bio}</p>
-        </div>
+          <div className='flex flex-col gap-4'>
+            <p className='text-page-title text-white'>.</p>
+            <p className='text-label text-bold font-scdream4'>{profile.nickname}</p>
+            <p className='text-label text-bold font-scdream4'>{profile.bio}</p>
+          </div>
 
-        <button onClick={handleEdit}>
-          <Edit sx={{ fontSize: 24, color: "#313866", marginRight: 2 }} />
-        </button>
+          <button onClick={handleEdit}>
+            <Edit sx={{ fontSize: 24, color: "#313866", marginRight: 2 }} />
+          </button>
+        </div>
       </div>
     </div>
   )
