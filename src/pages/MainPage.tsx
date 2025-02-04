6ded635cb73fb73ab12929a750b64bd27be06260
devdaradara <devdaradara@gmail.com>
2025-02-04 10:51:46 +0900

    feat: 네이버 기본 지도 로드 #39

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Map as KakaoMap } from "react-kakao-maps-sdk";
 import EventMarker from "../components/main/EventMarker";
 import StudyList from "../components/main/StudyList";
@@ -6,14 +6,28 @@ import { useGeolocation } from '../hooks/common/useGeolocation';
 import { useMeetingList } from "../hooks/api/useMeeting";
 
 const MainPage: React.FC = () => {
+  const mapRef = useRef<HTMLDivElement>(null);
   const [page, setPage] = useState(1); 
   const { location, loaded } = useGeolocation();
   const { data, isLoading } = useMeetingList(page);
-
+  
   const meetings = data?.data.content || [];
   const totalPages = data?.data?.totalPages ?? 0;
-
+  
   const canShowMap = loaded && location?.latitude != null && location?.longitude != null;
+
+  useEffect(() => {
+    if (!mapRef.current || !canShowMap || !window.naver) return;
+
+    const map = new naver.maps.Map(mapRef.current, {
+      center: new naver.maps.LatLng(location?.latitude, location?.longitude),
+      zoom: 17
+    });
+
+    return () => {
+      map?.destroy();
+    }
+  }, [canShowMap, location]);
 
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
@@ -29,7 +43,8 @@ const MainPage: React.FC = () => {
         totalPages={totalPages}
         onPageChange={handlePageChange}
       />
-      {canShowMap && (
+      <div ref={mapRef} id="map" style={{ width: "66%", height: "100%" }}/>
+      {/* {canShowMap && (
         <KakaoMap
           center={{ lat: location.latitude, lng: location.longitude }}
           style={{ width: "66%", height: "100%" }}
@@ -46,7 +61,7 @@ const MainPage: React.FC = () => {
             />
           ))}
         </KakaoMap>
-      )}
+      )} */}
     </div>
   );
 };
