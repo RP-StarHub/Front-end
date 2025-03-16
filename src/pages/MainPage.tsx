714cb6b5f50dafd0f08f67cf6ff98197d174313b
devdaradara <devdaradara@gmail.com>
2025-03-16 11:17:31 +0900

    chore: style 추가 #51

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,10 +7,10 @@ import { useMeetingList } from "../hooks/api/useMeeting";
 const MainPage: React.FC = () => {
   const mapRef = useRef<HTMLDivElement>(null);
   const [naverMap, setNaverMap] = useState<naver.maps.Map | null>(null);
-  const [page, setPage] = useState(1); 
+  const [page, setPage] = useState(1);
   const { location, loaded } = useGeolocation();
   const { data, isLoading } = useMeetingList(page);
-  
+
   const meetings = useMemo(() => data?.data.content || [], [data?.data.content]);
   const totalPages = data?.data?.totalPages ?? 0;
   const canShowMap = loaded && location?.latitude != null && location?.longitude != null;
@@ -44,27 +44,27 @@ const MainPage: React.FC = () => {
   return (
     <div className="w-full flex flex-col md:flex-row min-h-[90vh]">
       <div className="w-full md:w-1/3 lg:w-1/4">
-      <StudyList
-        meetings={meetings}
-        currentPage={page}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
+        <StudyList
+          meetings={meetings}
+          currentPage={page}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </div>
-       <div className="w-full md:w-2/3 lg:w-3/4 flex-grow h-[500px] md:h-auto">
-      <div ref={mapRef} id="map" style={{ width: "100%", position: "fixed", right: 0 }}>
-        {naverMap && meetings.map((meeting) => (
-          <EventMarker
-            key={meeting.id}
-            meeting={meeting}
-            position={{
-              latitude: meeting.latitude,
-              longitude: meeting.longitude
-            }}
-            map={naverMap}
-          />
-        ))}
-      </div>
+      <div className="w-full md:w-2/3 lg:w-3/4 flex-grow h-[500px] md:h-auto">
+        <div ref={mapRef} id="map" style={{ width: "100%", position: "fixed", right: 0 }}>
+          {naverMap && meetings.map((meeting) => (
+            <EventMarker
+              key={meeting.id}
+              meeting={meeting}
+              position={{
+                latitude: meeting.latitude,
+                longitude: meeting.longitude
+              }}
+              map={naverMap}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
