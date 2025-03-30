4ef4cb71c2ca2a829a4ae99c0d3c4652ec1cb065
devdaradara <devdaradara@gmail.com>
2025-03-30 20:15:28 +0900

    feat: 실제 백엔드 API 연동 #36

--- src/hooks/api/usePopular.ts
+++ src/hooks/api/usePopular.ts
@@ -5,55 +5,56 @@ import { popularService, mockPopularService } from "../../services/api/popular";
  * 프로젝트 인기글 목록을 조회하는 훅
  */
 export const usePopularProjects = () => {
-  // return useQuery({
-  //   queryKey: ['popularProjects'],
-  //   queryFn: () => popularService.getPopularProjects(),
-  //   select: (response) => response.data.data,
-  //   staleTime: 5 * 60 * 1000,
-  // });
-  
-  // 목업 
   return useQuery({
     queryKey: ['popularProjects'],
-    queryFn: () => mockPopularService.getPopularProjects(),
+    queryFn: () => popularService.getPopularProjects(),
     select: (response) => response.data.data,
+    staleTime: 5 * 60 * 1000,
   });
+  
+  // 목업 
+  // return useQuery({
+  //   queryKey: ['popularProjects'],
+  //   queryFn: () => mockPopularService.getPopularProjects(),
+  //   select: (response) => response.data.data,
+  // });
 };
 
 /**
  * 스터디 인기글 목록을 조회하는 훅
  */
 export const usePopularStudies = () => {
-  // return useQuery({
-  //   queryKey: ['popularStudies'],
-  //   queryFn: () => popularService.getPopularStudies(),
-  //   select: (response) => response.data.data,
-  //   staleTime: 5 * 60 * 1000,
-  // });
-  
-  // 목업 
   return useQuery({
     queryKey: ['popularStudies'],
-    queryFn: () => mockPopularService.getPopularStudies(),
+    queryFn: () => popularService.getPopularStudies(),
     select: (response) => response.data.data,
+    staleTime: 5 * 60 * 1000,
   });
+  
+  // 목업 
+  // eslint-disable-next-line no-unreachable
+  // return useQuery({
+  //   queryKey: ['popularStudies'],
+  //   queryFn: () => mockPopularService.getPopularStudies(),
+  //   select: (response) => response.data.data,
+  // });
 };
 
 /**
  * 마감임박 인기글 목록을 조회하는 훅
  */
 export const usePopularExpiring = () => {
-  // return useQuery({
-  //   queryKey: ['popularExpiring'],
-  //   queryFn: () => popularService.getPopularExpiring(),
-  //   select: (response) => response.data.data,
-  //   staleTime: 5 * 60 * 1000,
-  // });
-  
-  // 목업 
   return useQuery({
     queryKey: ['popularExpiring'],
-    queryFn: () => mockPopularService.getPopularExpiring(),
+    queryFn: () => popularService.getPopularExpiring(),
     select: (response) => response.data.data,
+    staleTime: 5 * 60 * 1000,
   });
+  
+  // 목업 
+  // return useQuery({
+  //   queryKey: ['popularExpiring'],
+  //   queryFn: () => mockPopularService.getPopularExpiring(),
+  //   select: (response) => response.data.data,
+  // });
 };
